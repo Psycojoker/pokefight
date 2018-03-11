daf3f5b5471470c3377b3049bedee5441d4bde09
Laurent Peuch <cortex@worlddomination.be>
2018-03-11 17:45:55 +0100

    [enh] do some verifications in the registration script

--- register.py
+++ register.py
@@ -1,9 +1,19 @@
+import sys
+from getpass import getpass
 from mastodon import Mastodon
 
-login_email = input("Bot login email (eg: username@domain.com): ").strip()
-account_password = input("Bot password (it won't be stored): ").strip()
-api_base_url = input("Url of the mastodon instance (eg: https://mastodon.social): ").strip()
-api_base_url = input("Url of the mastodon instance (eg: https://mastodon.social): ").strip()
+login_email = raw_input("Bot login email (eg: username@domain.com): ").strip()
+
+if "@" not in login_email:
+    print "The login email should be an email, not a username"
+    sys.exit(1)
+
+account_password = getpass("Bot password (it won't be stored): ").strip()
+api_base_url = raw_input("Url of the mastodon instance (eg: https://mastodon.social): ").strip()
+
+if not api_base_url.startswith(("https://", "http://")):
+    print "The url of the mastodon instance needs to start with either https:// or http://"
+    sys.exit(1)
 
 # Register app - only once!
 Mastodon.create_app(
